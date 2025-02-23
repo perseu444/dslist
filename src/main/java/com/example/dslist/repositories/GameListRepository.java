eaaa4550a91c3b3e36ece04826547766bb3f7bb6
Jader Marcelo <jadermarcelo95@gmail.com>
2025-02-23 19:14:07 -0300

    Get Lists

---
+++ src/main/java/com/example/dslist/repositories/GameListRepository.java
@@ -0,0 +1,10 @@
+package com.example.dslist.repositories;
+
+
+import com.example.dslist.entities.GameList;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameListRepository extends JpaRepository<GameList, Long> {
+
+
+}
