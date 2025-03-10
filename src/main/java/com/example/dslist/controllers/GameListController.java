87b79cb496a2129267f3bca0997e910d13b1d09f
Jader Marcelo <jadermarcelo95@gmail.com>
2025-03-09 23:53:04 -0300

    Replacing games around the list

--- src/main/java/com/example/dslist/controllers/GameListController.java
+++ src/main/java/com/example/dslist/controllers/GameListController.java
@@ -3,13 +3,11 @@ package com.example.dslist.controllers;
 import com.example.dslist.dto.GameDTO;
 import com.example.dslist.dto.GameListDTO;
 import com.example.dslist.dto.GameMinDTO;
+import com.example.dslist.dto.ReplacementDTO;
 import com.example.dslist.services.GameListService;
 import com.example.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,6 +32,13 @@ public class GameListController {
     public List<GameMinDTO> findByList(@PathVariable Long listId) {
         List<GameMinDTO> result = gameService.findByList(listId);
         return result;
+
+    }
+
+    @PostMapping(value = "/{listId}/replacement")
+    public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body) {
+        gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
+
 
     }
 
